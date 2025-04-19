37b490d97616c5dbe9b6f89c8fb461fcc07b6120
xJam-byte <abylaimuratbayev@gmail.com>
2025-04-19 09:32:02 +0500

    MyArrayList is done

---
+++ src/MyArrayList.java
@@ -0,0 +1,165 @@
+public class MyArrayList<T extends Comparable<T>> implements MyList<T> {
+    private Object[] data;
+    private int size;
+
+    public MyArrayList() {
+        data = new Object[10];
+        size = 0;
+    }
+
+    private void ensureCapacity() {
+        if (size == data.length) {
+            Object[] newData = new Object[data.length * 2];
+            System.arraycopy(data, 0, newData, 0, size);
+            data = newData;
+        }
+    }
+
+    private void checkBounds(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
+        }
+    }
+
+    @Override
+    public void add(T item) {
+        ensureCapacity();
+        data[size++] = item;
+    }
+
+    @Override
+    public void set(int index, T item) {
+        checkBounds(index);
+        data[index] = item;
+    }
+
+    @Override
+    public void add(int index, T item) {
+        if (index < 0 || index > size) {
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
+        }
+        ensureCapacity();
+        for (int i = size; i > index; i--) {
+            data[i] = data[i - 1];
+        }
+        data[index] = item;
+        size++;
+    }
+
+    @Override
+    public void addFirst(T item) {
+        add(0, item);
+    }
+
+    @Override
+    public void addLast(T item) {
+        add(item);
+    }
+
+    @Override
+    public T get(int index) {
+        checkBounds(index);
+        return (T) data[index];
+    }
+
+    @Override
+    public T getFirst() {
+        return get(0);
+    }
+
+    @Override
+    public T getLast() {
+        return get(size - 1);
+    }
+
+    @Override
+    public void remove(int index) {
+        checkBounds(index);
+        for (int i = index; i < size - 1; i++) {
+            data[i] = data[i + 1];
+        }
+        data[size - 1] = null;
+        size--;
+    }
+
+    @Override
+    public void removeFirst() {
+        remove(0);
+    }
+
+    @Override
+    public void removeLast() {
+        remove(size - 1);
+    }
+
+    @Override
+    public void sort() {
+        // Простая сортировка пузырьком
+        for (int i = 0; i < size - 1; i++) {
+            for (int j = 0; j < size - i - 1; j++) {
+                T a = (T) data[j];
+                T b = (T) data[j + 1];
+                if (a.compareTo(b) > 0) {
+                    data[j] = b;
+                    data[j + 1] = a;
+                }
+            }
+        }
+    }
+
+    @Override
+    public int indexOf(Object object) {
+        for (int i = 0; i < size; i++) {
+            if (data[i].equals(object)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    @Override
+    public int lastIndexOf(Object object) {
+        for (int i = size - 1; i >= 0; i--) {
+            if (data[i].equals(object)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    @Override
+    public boolean exists(Object object) {
+        return indexOf(object) != -1;
+    }
+
+    @Override
+    public Object[] toArray() {
+        Object[] result = new Object[size];
+        System.arraycopy(data, 0, result, 0, size);
+        return result;
+    }
+
+    @Override
+    public void clear() {
+        data = new Object[10];
+        size = 0;
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public java.util.Iterator<T> iterator() {
+        return new java.util.Iterator<>() {
+            int current = 0;
+            public boolean hasNext() {
+                return current < size;
+            }
+            public T next() {
+                return (T) data[current++];
+            }
+        };
+    }
+}
---
+++ src/MyList.java
@@ -0,0 +1,39 @@
+public interface MyList<T> {
+    void add(T item);
+
+    void set(int index, T item);
+
+    void add(int index, T item);
+
+    void addFirst(T item);
+
+    void addLast(T item);
+
+    T get(int index);
+
+    T getFirst();
+
+    T getLast();
+
+    void remove(int index);
+
+    void removeFirst();
+
+    void removeLast();
+
+    void sort();
+
+    int indexOf(Object object);
+
+    int lastIndexOf(Object object);
+
+    boolean exists(Object object);
+
+    Object[] toArray();
+
+    void clear();
+
+    int size();
+
+    java.util.Iterator<T> iterator();
+}
